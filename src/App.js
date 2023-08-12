d49fd0bfd3bec0cb0dda3b51ad3cb9898bea8237
Shaimaa-Ibrahim24 <shimaaebrahem24@gmail.com>
2023-08-12 20:52:45 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+
+
+
+function App() {
+  return (
+    <div>
+      <h1 className="tet">Shaimaa Ibrahim</h1>
+      <h2 className="tet">Sedra Ahmed</h2>
+    </div>
+  );
+}
+
+export default App;
